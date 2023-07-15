90eb9ad1c888612e3df4f75924c8c49a192680e8
natanlf <natanlaraferreira@gmail.com>
2023-07-15 18:18:38 +0000

    Projeto frontend next criado

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1> Welcome to the ConchaYOro App </h1>
+    </div>
+  )
+}
